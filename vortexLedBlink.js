182fad114a283a58b49258d8d112aedd3f77e127
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-02-02 01:48:14 -0300

    upgrade de version de socket io

--- vortexLedBlink.js
+++ vortexLedBlink.js
@@ -1,38 +1,50 @@
 var Vortex = require('vortexjs');
-var Gpio = require('onoff').Gpio;
-var led = new Gpio(17, 'out');
-var io = require('socket.io-client');
+//var Gpio = require('onoff').Gpio;
+//var led = new Gpio(17, 'out');
+var sck = require('socket.io-client')('https://router-vortex.herokuapp.com');
 
 var NodoRouter = Vortex.NodoRouter;
 var NodoConectorSocket = Vortex.NodoConectorSocket;
-var NodoPortalBidi = vx.NodoPortalBidi;
+var NodoPortalBidi = Vortex.NodoPortalBidi;
 
 var portal = new NodoPortalBidi();
 
-var connSocket = new NodoConectorSocket({
+sck.on("connect", function(){
+	console.log("conectado");
+}); 
+
+sck.on("connect_error", function(error){
+	console.log("error", error);
+}); 
+
+sck.on("connect_timeout", function(){
+	console.log("timeout");
+}); 
+
+var connSocket= new NodoConectorSocket({
 	id: "1",
-	socket: io.connect("https://router-vortex.herokuapp.com"), 
+	socket: sck, 
 	verbose: true, 
 	alDesconectar:function(){
 		console.log("Socket desconectado");
 	}
-});    
+});  
 
 portal.conectarCon(connSocket);
 connSocket.conectarCon(portal);
 
-portal.pedirMensajes({
+portal.pedirMensajes(new Vortex.FiltrosYTransformaciones.FiltroXEjemplo({
 	tipoDeMensaje:"raspi_vortex_led_blink.led_on"
-},function(msg){
-	led.writeSync(1);
+}),function(msg){
 	console.log("llego mensale de encendido");
+	led.writeSync(1);
 });
 
-portal.pedirMensajes({
+portal.pedirMensajes(new Vortex.FiltrosYTransformaciones.FiltroXEjemplo({
 	tipoDeMensaje:"raspi_vortex_led_blink.led_off"
-},function(msg){
-	led.writeSync(0);
+}),function(msg){
 	console.log("llego mensale de apagado");
+	led.writeSync(0);
 });
 
 console.log("comenzando");
